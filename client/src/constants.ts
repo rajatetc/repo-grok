8dcfc997e90722f7280ec069be9fe060d44b301d
Rajat <rajatgupta1798@gmail.com>
2026-05-13 15:59:03 +0530

    chore(examples): drop TanStack query, keep 5 examples

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,10 +1,9 @@
 export const EXAMPLES = [
-  { owner: "immerjs",    repo: "immer"         },
-  { owner: "reduxjs",    repo: "redux"         },
-  { owner: "pmndrs",     repo: "zustand"       },
-  { owner: "TanStack",   repo: "query"         },
-  { owner: "axios",      repo: "axios"         },
-  { owner: "colinhacks", repo: "zod"           },
+  { owner: "immerjs",    repo: "immer"   },
+  { owner: "reduxjs",    repo: "redux"   },
+  { owner: "pmndrs",     repo: "zustand" },
+  { owner: "axios",      repo: "axios"   },
+  { owner: "colinhacks", repo: "zod"     },
 ];
 
 export const API_BASE = import.meta.env.VITE_API_URL ?? "http://localhost:3001";
