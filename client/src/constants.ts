53cd1a2aac5a25ef1944cd4c27f12ee0469dbb11
Rajat <rajatgupta1798@gmail.com>
2026-05-13 15:59:21 +0530

    chore(examples): add express at #3, move immer to #5, drop zod

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,9 +1,9 @@
 export const EXAMPLES = [
-  { owner: "immerjs",    repo: "immer"   },
   { owner: "reduxjs",    repo: "redux"   },
   { owner: "pmndrs",     repo: "zustand" },
+  { owner: "expressjs",  repo: "express" },
   { owner: "axios",      repo: "axios"   },
-  { owner: "colinhacks", repo: "zod"     },
+  { owner: "immerjs",    repo: "immer"   },
 ];
 
 export const API_BASE = import.meta.env.VITE_API_URL ?? "http://localhost:3001";
